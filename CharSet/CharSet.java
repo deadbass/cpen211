9ddad9f321de2abb1c22708df054e269f04f7cb5
Rory Fraser <roryf@dccnet.com>
2015-10-31 00:18:15 -0700

    finished charset

--- CharSet/CharSet.java
+++ CharSet/CharSet.java
@@ -12,12 +12,36 @@ public class CharSet {
     
     @Override
     public boolean equals(Object obj) {
-        return false; // Replace with your implementation
+        if (this == obj){
+            return true;
+        }
+        
+        if (obj instanceof String){
+            String thisstring = (String) obj;
+            int n = s.length();
+            if (n == thisstring.length()){
+                char[] chara1 = s.toCharArray();
+                char[] chara2 = thisstring.toCharArray();
+                
+                //given that they're supposed to be sorted in non-descending order, each index entry should be the same
+                for (int i = 0; i < s.length(); i++){
+                    if (chara1[i] != chara2[i]){
+                        return false;
+                    }
+                }
+                return true;
+            }
+            
+        }
+        return false; 
     }
     
     @Override
     public int hashCode() {
-        return 0; // Replace with your implementation
-    }
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((s == null) ? 0 : s.hashCode());
+        return result;
 
+}
 }
